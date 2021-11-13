1105c1cfd71df7b0351aece3e6590a473afb4d9c
Aidan Amato <aidanamato@comcast.net>
2021-11-13 13:24:15 -0500

    add resume link

--- src/components/Main/Resume/index.js
+++ src/components/Main/Resume/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import resumePdf from '../../../assets/aidan_amato_resume.pdf';
 
 function Resume() {
   return (
     <section id="resume" className="resume">
-        
+        <h2 className="section-title">Resume</h2>
+        <a href={resumePdf} target="_blank" rel="noreferrer">
+          <div className="resume-card">
+            <p>Resume Download</p>
+            <i className="far fa-file-pdf"></i>
+          </div>
+        </a>
     </section>
   );
 }
